b871af0a502a5cf26c2b60e3081f1994bbb8f699
yatharth1cr <yatharthgiri187@gmail.com>
2024-08-31 00:19:44 +0530

    created an Article-App

---
+++ Component-props/article-app/src/components/footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer className="flex">
+      <h2>2024 All Rights Reserved</h2>
+      <ul className="flex">
+        <li>Facebook</li>
+        <li>Twitter</li>
+        <li>Instagram</li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Component-props/article-app/src/components/header.js
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <div>
+      <header className="flex">
+        <h1>Logo</h1>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
